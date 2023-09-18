15a40362984db53ac066a72b241d69b50509d1b8
rachel000703 <rachel000703@naver.com>
2023-09-18 18:36:57 +0900

    first commit

---
+++ app/src/main/java/com/example/motivewatch/WatchAlarm.java
@@ -0,0 +1,14 @@
+package com.example.motivewatch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WatchAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.watch_alarm);
+    }
+}
